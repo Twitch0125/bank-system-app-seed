4935306a600ad46dc33faf4e1849ba4f7fbf10c6
Kaleb Ercanbrack <twitcherc@gmail.com>
2019-07-18 10:31:24 -0600

    implemented checking account

--- helper/AccountFactory.ts
+++ helper/AccountFactory.ts
@@ -1,17 +1,19 @@
-import {Account} from "../common/interfaces/Account";
+import { Account } from "../common/interfaces/Account";
+import { CheckingAccount } from "../student-work/CheckingAccount";
 
 export class AccountFactory {
+  static getCheckingAccountObject(currentDate: Date): Account {
+    return new CheckingAccount(currentDate);
+  }
 
-    static getCheckingAccountObject(currentDate: Date): Account {
-        throw new Error("You need to implement this :)");
-    }
+  static getSavingsAccountObject(currentDate: Date): Account {
+    throw new Error("You need to implement this :)");
+  }
 
-    static getSavingsAccountObject(currentDate: Date): Account {
-        throw new Error("You need to implement this :)");
-    }
-
-    static getRetirementAccountObject(currentDate: Date, accountHolderBirthDate: Date): Account {
-        throw new Error("You need to implement this :)");
-    }
-
+  static getRetirementAccountObject(
+    currentDate: Date,
+    accountHolderBirthDate: Date
+  ): Account {
+    throw new Error("You need to implement this :)");
+  }
 }
